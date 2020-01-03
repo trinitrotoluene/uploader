4bfb3a46874ae81921c67c2ce0718cc6e2a5904f
Trinitrotoluene <tnt@codeforge.cc>
2020-01-03 20:27:17 +0000

    Better error messages

--- app.py
+++ app.py
@@ -22,8 +22,11 @@ def showfile(filename):
 @app.route("/upload", methods=["POST"])
 def upload():
     # Validate Method and Content-Type
-    if not request.method == "POST" or not request.content_type.split(";")[0] == "multipart/form-data":
-        return "Bad Request", 400
+    if not request.method == "POST":
+        return "Bad method", 400
+    if not request.content_type.split(";")[0] == "multipart/form-data":
+        return "Bad mime-type", 400
+
     # Check for existence of a key in request headers
     if not request.headers.has_key("Authorization"):
         return "Unauthorized", 401 
@@ -38,12 +41,12 @@ def upload():
                 # Log which user uploaded the file
     
     if not valid:
-        return "Forbidden", 403
+        return "Bad key", 403
     
     # Try to load image
     filecontent = request.files["upload"]
     if not is_allowed_file(filecontent.filename):
-        return "Bad Request", 400
+        return "Bad filetype", 400
     
     try:
         # Strip EXIF by loading and resaving the image
@@ -57,13 +60,13 @@ def upload():
         if is_safe_path(targetpath):
             image.save(targetpath)
         else:
-            return "Bad Request", 400
+            return "Bad path", 400
         # Return JSON with URL
         return json.dumps({
             "url": urllib.parse.urljoin(request.host_url, filename)
         }), 200
     except:
-        return "Bad Request", 400
+        return "Bad request", 400
    
 
 if __name__ == "__main__":
